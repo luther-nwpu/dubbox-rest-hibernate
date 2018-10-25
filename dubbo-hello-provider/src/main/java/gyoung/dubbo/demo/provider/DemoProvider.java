fb4587f932d6bcf064710195004c55790530ec91
张露 <2424733678@qq.com>
2018-10-25 17:59:15 +0800

    finish the dubbox

---
+++ dubbo-hello-provider/src/main/java/gyoung/dubbo/demo/provider/DemoProvider.java
@@ -0,0 +1,18 @@
+package gyoung.dubbo.demo.provider;
+
+/**
+ * Created by zengjiyang on 2016/2/16.
+ */
+import org.apache.log4j.BasicConfigurator;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import java.io.IOException;
+
+public class DemoProvider {
+    public static void main(String[] args) throws IOException {
+        BasicConfigurator.configure();
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath*:dubbo-demo-provider.xml");
+        context.start();
+        System.out.println("服务已经启动...");
+        System.in.read();
+    }
+}
